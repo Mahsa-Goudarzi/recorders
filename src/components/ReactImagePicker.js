86a07e350f1459f70f9add2f50af582e9dae23ce
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2023-12-30 18:15:33 +0330

    use a library to take pictures

--- src/components/ReactImagePicker.js
+++ src/components/ReactImagePicker.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
 
-export default function ReactImagePicker() {
-  return <div>Image Picker</div>;
+//import ImagePreview from "./ImagePreview"; // source code : ./src/demo/ReactImagePickerWithImagePreview/ImagePreview
+
+function ReactImagePicker() {
+  const [dataUri, setDataUri] = useState("");
+
+  function handleTakePhotoAnimationDone(dataUri) {
+    console.log("takePhoto");
+    setDataUri(dataUri);
+  }
+
+  const isFullscreen = false;
+  return (
+    <div>
+      {dataUri ? (
+        <img src={dataUri} alt="some pic" />
+      ) : (
+        <Camera
+          onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+          isFullscreen={isFullscreen}
+        />
+      )}
+    </div>
+  );
 }
+
+export default ReactImagePicker;
